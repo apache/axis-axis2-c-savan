fae07dfb28cc8a8075d69026e5e918b51c1d9d08
Sanjaya Ratnaweera <pini@apache.org>
2007-06-20 06:02:17 +0000

    Fixed build break

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include/axis2-1.0 --with-axiom=${AXIS2C_HOME}/include/axis2-1.0
+./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2_util=${AXIS2C_HOME}/include/axis2-1.1 --with-axiom=${AXIS2C_HOME}/include/axis2-1.1
 make
