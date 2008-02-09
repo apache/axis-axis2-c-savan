e6e210b2a93b2a4ebe2897d2d5eacd1c7c653a79
Damitha N.M. Kumarage <damitha@apache.org>
2008-02-09 04:13:36 +0000

    Updating the build files

--- samples/build.sh
+++ samples/build.sh
@@ -2,6 +2,6 @@
 
 ./autogen.sh
 
-./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.1 --with-savan=${AXIS2C_HOME}/include/savan-0.90
+./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.3.0 --with-savan=${AXIS2C_HOME}/include/savan-0.90
 make -j30
 
