b2c03c82df9b717192ef6596ca66a598f893b81d
Sanjaya Ratnaweera <pini@apache.org>
2008-05-02 03:01:22 +0000

    Changed axis2/c version to 1.4.0

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=$AXIS2C_HOME --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.3.1 --enable-filtering=no
+./configure --prefix=$AXIS2C_HOME --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.4.0 --enable-filtering=no
 make -j30
 make install
 
 cd samples
 sh autogen.sh
-./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.3.1 --with-savan=${AXIS2C_HOME}/include/savan-0.90
+./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.4.0 --with-savan=${AXIS2C_HOME}/include/savan-0.90
 make -j10
 make install
 cd ..
