c00142a293e9263a103c256d4c78e6634ef8177f
Damitha N.M. Kumarage <damitha@apache.org>
2008-07-12 02:14:30 +0000

    updating the build for AXIS2C 1.5.0

--- samples/build.sh
+++ samples/build.sh
@@ -2,6 +2,6 @@
 
 ./autogen.sh
 
-./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.4.0 --with-savan=${AXIS2C_HOME}/include/savan-0.90
+./configure --prefix=${AXIS2C_HOME} --with-axis2=${AXIS2C_HOME}/include/axis2-1.5.0 --with-savan=${AXIS2C_HOME}/include/savan-0.90
 make -j10
 make install
